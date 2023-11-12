f6c143769538a6c7fba41951601b4a708ac82538
Ananthakrishnanks1 <ananthakrishnanks18@gmail.com>
2023-11-12 07:20:06 +0530

    added auth

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+const String APPWRITE_PROJECT_ID = "654fb47819e60441c148";
+const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
+
+const String APPWRITE_DATABASE_ID = "65502d9021b8d509cd0a";
+const String COLLECTION_MESSAGES = "";
